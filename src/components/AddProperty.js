c36c46365b5fa7fd9f813836da7a2bc4a1bffc23
Abi Turner <turner.abi@gmail.com>
2023-04-08 16:43:09 +0100

    created 'add property' form and updated test

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/add-property.css";
 
 const AddProperty = () => {
-  return <div>Add Property Page</div>;
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    // eslint-disable-next-line no-console
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="add-property">
+      Add Property Page
+      <form onSubmit={handleAddProperty}>
+        <div className="form-field">
+          <label htmlFor="title">
+            Title
+            <input
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="city">
+            City
+            <select
+              id="city"
+              name="city"
+              value={fields.city}
+              onChange={handleFieldChange}
+            >
+              <option value="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Liverpool">Liverpool</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="type">
+            Type
+            <select
+              id="type"
+              name="type"
+              value={fields.type}
+              onChange={handleFieldChange}
+            >
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Cottage">Cottage</option>
+              <option value="Bungalow">Bungalow</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="bedrooms">
+            Bedrooms
+            <input
+              id="bedrooms"
+              name="bedrooms"
+              value={fields.bedrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="bathrooms">
+            Bathrooms
+            <input
+              id="bathrooms"
+              name="bathrooms"
+              value={fields.bathrooms}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="price">
+            Price{" "}
+            <input
+              id="price"
+              name="price"
+              value={fields.price}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="email">
+            Email
+            <input
+              name="email"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+          </label>
+        </div>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProperty;
--- src/tests/AddProperty.test.js
+++ src/tests/AddProperty.test.js
@@ -1,18 +1,12 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import App from "../components/App";
-
-// test("renders app correctly", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Surreal Estate/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import AddProperty from "../components/AddProperty";
 
 describe("App", () => {
   const { asFragment } = render(
     <BrowserRouter basename="/">
-      <App />
+      <AddProperty />
     </BrowserRouter>
   );
 
